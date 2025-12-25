f97d2ea3ac80052d87b4eda89a8f361b8b02a21f
shivam-kumar-01 <shivam1f.aps@gmail.com>
2025-12-26 01:16:25 +0530

    done with the website backend part and the styling part of listings ,show page,edit page and new page

---
+++ models/listing.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const listingSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: String,
+    url: String,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+module.exports = mongoose.model("Listing", listingSchema);
