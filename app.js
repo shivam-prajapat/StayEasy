f1c51121a0843fc3eadefcca28840487064ad195
shivam-kumar-01 <shivam1f.aps@gmail.com>
2025-12-27 09:43:37 +0530

    make changes for user validation so that they can't send a blank input

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ main()
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine('ejs',ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine('ejs',ejsMate);
+
 app.use(express.static(path.join(__dirname,"/public")));
 
 
